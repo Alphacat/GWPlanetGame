fb491a7cf39c8291619c2bc310685ed95160da81
Paul Sander <paulleosander@gmail.com>
2012-03-04 14:54:56 -0800

    Redid Map.cs to use new camera system.

--- GWPlanetGame/GWPlanetGame/Map.cs
+++ GWPlanetGame/GWPlanetGame/Map.cs
@@ -41,68 +41,27 @@ namespace GWPlanetGame
 
         }
 
-        public void DrawView(SpriteBatch spriteBatch, Texture2D texture, Camera camera, Point point)
+        public void DrawView(SpriteBatch spriteBatch, Texture2D texture )
         {
-            // Start at the upper left corner of the drawing area and work our
-            //  way across.
+            // The old method examined everything in the array and figured out its destination.
+            // Instead, we're now going to use the camera transform to take care of this logic
+            // for us, and instead just draw everything without regard for where it goes.
 
             // Helper variables
-            int tileX = 0;  // These are the x and y coordinates of the tile we're working with
-            int tileY = 0;
-
             TileID drawTile;    // What tile are we drawing?
 
-            Rectangle destRect, sourceRect;
+            Rectangle sourceRect;
 
-            // Bounds for the tile we're drawing.
-            int leftbound, upbound, rightbound, downbound;
-
-            for( int x = camera.X; x <= camera.X + camera.Width; x += Tiles.TILE_WIDTH )
-                for (int y = camera.Y; y <= camera.Y + camera.Height; y += Tiles.TILE_HEIGHT)
+            for ( int x = 0; x < mapWidth; x ++ )
+                for (int y = 0; y < mapHeight; y++)
                 {
-                    // First step, find out which tile we're dealing with.
-                    tileX = (x >= 0) ? (x / Tiles.TILE_WIDTH) : (x / Tiles.TILE_WIDTH - 1);
-                    tileY = (y >= 0) ? (y / Tiles.TILE_WIDTH) : (y / Tiles.TILE_WIDTH - 1);
+                    drawTile = data[x,y];
 
-                    // Is this tile actually in our map?
-                    if ( 0 <= tileX && tileX < mapWidth && 0 <= tileY && tileY < mapHeight )
-                        drawTile = data[tileX, tileY];
-                    else
-                        // Default for now will be the black tile.
-                        drawTile = TileID.BLACK;
-
-                    // Now that we have what tile we're drawing, we need to know what portion to draw.
-                    // We'll check the tile's bounds against our camera rectangle.
-                    // Nope. We're going to draw the whole thing for now, just to make it easier.
-                    // ( I don't think bleeding over the edge of the screen a little will hurt. )
-                    // Left bound
-                    //leftbound = ( (tileX * Tiles.TILE_WIDTH) < camera.X) ? camera.X % Tiles.TILE_WIDTH : 0;
-                    leftbound = 0;
-                    // Upper bound
-                    //upbound = ( (tileY * Tiles.TILE_HEIGHT) < camera.Y) ? camera.Y % Tiles.TILE_HEIGHT : 0;
-                    upbound = 0;
-                    // Right bound
-                    //rightbound = (((tileX + 1) * Tiles.TILE_WIDTH) > camera.X + camera.Width) ?
-                    //    camera.X % Tiles.TILE_WIDTH : Tiles.TILE_WIDTH;
-                    rightbound = Tiles.TILE_HEIGHT;
-                    // Lower bound
-                    //downbound = (((tileY + 1) * Tiles.TILE_HEIGHT) > camera.Y + camera.Height) ?
-                     //   camera.Y % Tiles.TILE_HEIGHT : Tiles.TILE_HEIGHT;
-                    downbound = Tiles.TILE_HEIGHT;
-
-                    destRect = new Rectangle(tileX * Tiles.TILE_WIDTH - camera.X + point.X,
-                                                            tileY * Tiles.TILE_HEIGHT - camera.Y + point.Y,
-                                                            rightbound - leftbound,
-                                                            downbound - upbound);
-                    sourceRect = new Rectangle(Tiles.TILE_WIDTH * (int)drawTile + leftbound,
-                                                            0,
-                                                            rightbound - leftbound,
-                                                            downbound - upbound);
-
-                    // Now that we've figured out the bounds of the tile to draw, we need to grab our
-                    //  tile from the source, and draw it to the screen.
-                    spriteBatch.Draw(texture, destRect, sourceRect, Color.White);
-                    // Whew! Now on to the next loop.
+                    // Build a sourcerect for the tile
+                    sourceRect = new Rectangle(Tiles.TILE_WIDTH * (int)drawTile, 0,
+                        Tiles.TILE_WIDTH, Tiles.TILE_HEIGHT);
+                    spriteBatch.Draw(texture, new Vector2(x * Tiles.TILE_WIDTH, y * Tiles.TILE_HEIGHT),
+                        sourceRect, Color.White);
                 }
         }
     }
