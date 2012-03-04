d33f793315d2dfb4434ba6ed6efb8674ee58a6cd
Paul Sander <paulleosander@gmail.com>
2012-03-04 14:12:21 -0800

    Complete overhaul of camera class to use a transformation matrix. Next is to modify the rest of the code to use this.

--- GWPlanetGame/GWPlanetGame/Camera.cs
+++ GWPlanetGame/GWPlanetGame/Camera.cs
@@ -10,44 +10,30 @@ namespace GWPlanetGame
     /// </summary>
     class Camera
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        private Vector2 mapPoint, screenSize;
+        // Reference to the current game instance
+        private Game _game;
+
+        // Camera's position
+        private Vector2 _point;
+
+        // Camera's transform property.
+        private Matrix _matrixtransform;
+
+        // Camera's zoom value.
+        private float _zoom;
 
         #region Constructors
         /// <summary>
-        /// Constructor to set camera state based off an existing rectangle
+        /// Constructor for our camera class.
         /// </summary>
-        /// <param name="rectangle">Rectangle representing initial coordinate and screen size.</param>
-        public Camera(Rectangle rectangle)
+        /// <param name="game"></param>
+        public Camera( Game game )
         {
-            mapPoint = new Vector2( (float) rectangle.X, (float) rectangle.Y);
-            screenSize = new Vector2( (float) rectangle.Width, (float) rectangle.Height);
-        }
+            _game = game;
+            _point = new Vector2( 0.0f, 0.0f );
+            _zoom = 1.0f;
 
-        /// <summary>
-        /// Constructor to set camera state at (X,Y) = (0,0) by default, with width and height.
-        /// </summary>
-        /// <param name="width">Width of screen in pixels.</param>
-        /// <param name="height">Height of screen in pixels.</param>
-        public Camera(int width, int height)
-        {
-            mapPoint = new Vector2(0, 0);
-            screenSize = new Vector2((float)width, (float)height);
-        }
-
-        /// <summary>
-        /// Constructor to set camera state with four float parameters.
-        /// </summary>
-        /// <param name="x">Initial x camera point.</param>
-        /// <param name="y">Initial y camera point.</param>
-        /// <param name="width">Initial screen width.</param>
-        /// <param name="height">Initial screen height.</param>
-        public Camera(float x, float y, float width, float height)
-        {
-            mapPoint = new Vector2(x, y);
-            screenSize = new Vector2(width, height);
+            UpdateMatrix();
         }
         #endregion
 
@@ -59,7 +45,7 @@ namespace GWPlanetGame
         /// whole pixel amounts.
         /// </summary>
         /// <returns>A rectangle representing the camera's position and area smoothed to whole integers.</returns>
-        Rectangle getRectangle()
+        public Rectangle getRectangle()
         {
             Rectangle tempRect = new Rectangle();
             // Here we round down (left and up, respectively).
@@ -70,38 +56,44 @@ namespace GWPlanetGame
             tempRect.Height = (int)Math.Ceiling(Height);
             return tempRect;
         }
+
+        /// <summary>
+        /// Recalculate the camera's transformation matrix. Use this
+        /// any time the camera's position or zoom factor is altered.
+        /// TODO: Make this respond to window size events.
+        /// </summary>
+        private void UpdateMatrix()
+        {
+            _matrixtransform = Matrix.CreateTranslation(new Vector3(-X, -Y, 0)) *
+                Matrix.CreateScale(new Vector3(_zoom, _zoom, 0)) *
+                Matrix.CreateTranslation(new Vector3(Width / 2, Height / 2, 0));
+        }
         #endregion
 
         #region Properties
         /// <summary>
-        /// Property to get and set the camera width manually. 
-        /// TODO: Has no effect on actual screen size.
+        /// Property to get the camera width manually.
         /// </summary>
         public float Width
         {            
-            get { return screenSize.X; }
-            set { screenSize.X = value; }
+            get { return _game.GraphicsDevice.Viewport.Width; }
         }
 
         /// <summary>
-        /// Property to get and set the camera height manually.
-        /// TODO: Has no effect on actul screen size.
+        /// Property to get the camera height manually.
         /// </summary>
         public float Height
         {
-            get { return screenSize.Y; }
-            set { screenSize.Y= value; }
+            get { return _game.GraphicsDevice.Viewport.Height; }
         }
 
         /// <summary>
-        /// Property to get and set the camera size manually, represented as
+        /// Property to get the camera size manually, represented as
         /// an (X,Y) coordinate pair by a Vector2.
-        /// TODO: Has no effect on actual screen size.
         /// </summary>
         public Vector2 ScreenSize
         {
-            get { return screenSize; }
-            set { screenSize = value; }
+            get { return new Vector2(Width, Height); }
         }
 
         /// <summary>
@@ -109,8 +101,8 @@ namespace GWPlanetGame
         /// </summary>
         public float X
         {
-            get { return mapPoint.X; }
-            set { mapPoint.X = value; }
+            get { return _point.X; }
+            set { _point.X = value; UpdateMatrix(); }
         }
 
         /// <summary>
@@ -118,18 +110,18 @@ namespace GWPlanetGame
         /// </summary>
         public float Y
         {
-            get { return mapPoint.Y; }
-            set { mapPoint.Y = value; }
+            get { return _point.Y; }
+            set { _point.Y = value; UpdateMatrix(); }
         }
 
         /// <summary>
         /// Property to get and set the camera's position as
         /// an (X,Y) coordinate pair represented by a Vector2.
         /// </summary>
-        public Vector2 Position
+        public Vector2 Point
         {
-            get { return mapPoint; }
-            set { mapPoint = value; }
+            get { return _point; }
+            set { _point = value; UpdateMatrix(); }
         }
 
         /// <summary>
@@ -141,9 +133,20 @@ namespace GWPlanetGame
         {
             get { return new Vector2( X + Width / 2, Y + Height / 2 ); }
             set {
-                mapPoint.X = ( value.X - screenSize.X/2 );
-                mapPoint.Y = ( value.Y - screenSize.Y/2 );
+                // Make sure we don't do this via properties, so we don't
+                // have two matrix recalculations.
+                _point.X = ( value.X - Width/2 );
+                _point.Y = ( value.Y - Height/2 );
+                UpdateMatrix();
             }
+        }
+
+        /// <summary>
+        /// Fetch the camera's transform matrix.
+        /// </summary>
+        public Matrix Transform
+        {
+            get { return _matrixtransform; }
         }
         #endregion
     }
