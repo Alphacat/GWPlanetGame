6f3111d5630dcf9f70fe03aa90f9f88a4eab05aa
Paul Sander <paulleosander@gmail.com>
2012-03-17 15:26:41 -0700

    Added chunk class for managing terrain chunks.

---
+++ GWPlanetGame/GWPlanetGame/Chunk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GWPlanetGame
+{
+    /// <summary>
+    /// Class for managing verticalslices of the terrain.
+    /// In a really rough state at the moment and needs more polish.
+    /// </summary>
+    class Chunk
+    {
+        public TileID _tile;
+
+        public Chunk()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Texture2D terrainTexture, int X, int Y)
+        {
+            spriteBatch.Draw(terrainTexture,
+                new Vector2(X, Y),
+                new Rectangle(Tiles.TILE_WIDTH * (int)_tile, 0,
+                    Tiles.TILE_WIDTH, Tiles.TILE_HEIGHT),
+                    Color.White);
+        }
+    }
+}
