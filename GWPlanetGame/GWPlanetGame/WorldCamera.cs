7361607324004fd33f4b1cc4323e4efa2a7aea49
Paul Sander <paulleosander@gmail.com>
2012-03-17 15:31:18 -0700

    Removed custom getRectangle() function to use default. Changed matrix transform to place coordinates back at upper-left corner of the screen.

--- GWPlanetGame/GWPlanetGame/WorldCamera.cs
+++ GWPlanetGame/GWPlanetGame/WorldCamera.cs
@@ -9,27 +9,7 @@ namespace GWPlanetGame
         public WorldCamera(GraphicsDevice graphicsDevice)
             : base(graphicsDevice)
         { }
-
-        /// <summary>
-        /// Returns a rectangle representing the camera's position
-        /// and size smoothed out to integer values. This is to
-        /// assist in grabbing textures, which SpriteBatch wants in
-        /// whole pixel amounts.
-        /// TODO: Redo the math if the zoom feature is implemented.
-        /// </summary>
-        /// <returns>A rectangle representing the camera's position and area smoothed to whole integers.</returns>
-        public override Rectangle getRectangle()
-        {
-            Rectangle tempRect = new Rectangle();
-            // Here we round down (left and up, respectively).
-            tempRect.X = (int)Math.Floor(X - Width / 2);
-            tempRect.Y = (int)Math.Floor(Y - Height / 2);
-            // Here we round up (right and down, respectively).
-            tempRect.Width = (int)Math.Ceiling(X + Width / 2);
-            tempRect.Height = (int)Math.Ceiling(Y + Height / 2);
-            return tempRect;
-        }
-
+        
         /// <summary>
         /// Used for drawing objects using world coordinates.
         /// This also reorients the camera's orientation to
@@ -38,9 +18,10 @@ namespace GWPlanetGame
         /// </summary>
         protected override void UpdateMatrix()
         {
-            _matrixtransform = Matrix.CreateTranslation(new Vector3(-X, -Y, 0)) *
-                Matrix.CreateScale(new Vector3(_zoom, _zoom, 0)) *
-                Matrix.CreateTranslation(new Vector3(Width / 2, Height / 2, 0));
+            //_matrixtransform = Matrix.CreateTranslation(new Vector3(-X, -Y, 0)) *
+            //    Matrix.CreateScale(new Vector3(_zoom, _zoom, 0)) *
+            //    Matrix.CreateTranslation(new Vector3(Width / 2, Height / 2, 0));
+            _matrixtransform = Matrix.CreateTranslation(new Vector3(-X, -Y, 0));
         }
     }
 }
