0ef834697d71e3c2266095fbd55f2e4aca56af39
Paul Sander <paulleosander@gmail.com>
2012-05-09 15:03:14 -0700

    Added EditorViewControl to Form1 for testing

--- HephaestusEditor/Form1.Designer.cs
+++ HephaestusEditor/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.editorViewControl1 = new HephaestusEditor.Controls.EditorViewControl();
+            this.SuspendLayout();
+            // 
+            // editorViewControl1
+            // 
+            this.editorViewControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editorViewControl1.Location = new System.Drawing.Point(0, 0);
+            this.editorViewControl1.Name = "editorViewControl1";
+            this.editorViewControl1.Size = new System.Drawing.Size(284, 262);
+            this.editorViewControl1.TabIndex = 0;
+            this.editorViewControl1.Text = "editorViewControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.editorViewControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Controls.EditorViewControl editorViewControl1;
     }
 }
 
