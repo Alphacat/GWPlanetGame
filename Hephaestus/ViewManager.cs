277754f94b33d29e9c66ed2e247a9ea603c186b9
Paul Sander <paulleosander@gmail.com>
2012-05-09 12:07:17 -0700

    Initial commit of ViewManager to test editor contorl code with.

---
+++ Hephaestus/ViewManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Hephaestus.Views;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hephaestus
+{
+    static class ViewManager
+    {
+        static List<EditorView> _views;
+
+        static public ViewManager()
+        {
+            _views = new List<EditorView>();
+        }
+
+        public EditorView GetEditorView( GraphicsDevice graphicsDevice )
+        {
+            EditorView view = new EditorView(graphicsDevice);
+            _views.Add(view);
+            return view;
+        }
+    }
+}
