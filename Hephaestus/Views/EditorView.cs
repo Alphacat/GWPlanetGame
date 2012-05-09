5b7c3811b153edb42b90bb5b96b6f2c64ed5f14d
Paul Sander <paulleosander@gmail.com>
2012-05-09 12:00:35 -0700

    Initial commit of EditorView.cs

---
+++ Hephaestus/Views/EditorView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hephaestus.Views
+{
+    class EditorView
+    {
+        GraphicsDevice _graphicsDevice;
+
+        public EditorView(GraphicsDevice graphicsDevice)
+        {
+            _graphicsDevice = graphicsDevice;
+        }
+
+        public void Update(float elapsedTime)
+        {
+            _graphicsDevice.Clear(Microsoft.Xna.Framework.Color.Beige);
+        }
+
+        public void Draw(float elapsedTime)
+        {
+        }
+    }
+}
