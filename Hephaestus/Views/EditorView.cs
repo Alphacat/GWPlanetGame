6d1a966642e4de0faec11e6d9761b96e55d1a3cf
Paul Sander <paulleosander@gmail.com>
2012-05-20 18:15:38 -0700

    Added GameView alongside EditorView and linked them with a base class.

--- Hephaestus/Views/EditorView.cs
+++ Hephaestus/Views/EditorView.cs
@@ -4,21 +4,45 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Hephaestus.Views
 {
-    public class EditorView
+    abstract public class EngineView
     {
-        GraphicsDevice _graphicsDevice;
+        protected GraphicsDevice _graphicsDevice;
 
+        abstract public void Update(float elapsedTime);
+
+        abstract public void Draw(float elapsedTime);
+    }
+
+    public class EditorView : EngineView
+    {
         public EditorView(GraphicsDevice graphicsDevice)
         {
             _graphicsDevice = graphicsDevice;
         }
 
-        public void Update(float elapsedTime)
+        public override void Update(float elapsedTime)
         {
             _graphicsDevice.Clear(Microsoft.Xna.Framework.Color.Cyan);
         }
 
-        public void Draw(float elapsedTime)
+        public override void Draw(float elapsedTime)
+        {
+        }
+    }
+
+    public class GameView : EngineView
+    {
+        public GameView(GraphicsDevice graphicsDevice)
+        {
+            _graphicsDevice = graphicsDevice;
+        }
+
+        public override void Update(float elapsedTime)
+        {
+            _graphicsDevice.Clear(Microsoft.Xna.Framework.Color.DarkBlue);
+        }
+
+        public override void Draw(float elapsedTime)
         {
         }
     }
